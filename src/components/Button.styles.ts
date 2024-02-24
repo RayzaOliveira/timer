9b8d25773d0c6c03ccc0e549176a6999c149ead1
RayzaOliveira <rayza.ocr@gmail.com>
2024-02-24 18:48:10 -0300

    feat: Estrutura da aplicação - Tipagem de temas

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,11 +16,17 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
 
   // interpolação, vai funcionar como função
-  ${(props) => {
+  /* ${(props) => {
     return css`
       background-color: ${buttonVariants[props.variant]};
     `;
-  }}
+  }} */
 `;
